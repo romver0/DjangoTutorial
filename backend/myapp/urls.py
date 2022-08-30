a133e7ff20031c143e3409f3d74f37686463449f
Romver <roma_zverev2002@mail.ru>
2022-08-30 22:41:08 +0300

    Произошли изменения

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include, re_path
-from backend.myapp import views
+# from backend.myapp import views
+from myapp.views import *
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', index, name='home'),
     # path('car/<int:car_id>/',views.categories)
-    path('cars/<slug:type>/', views.categories, name='cars'),
-    re_path(r'archive/(?P<year>[0-9]{4})/', views.archive, name='archive'), #для регулярок
+    path('cars/<slug:type>/', categories, name='cars'),
+    re_path(r'archive/(?P<year>[0-9]{4})/', archive, name='archive'), #для регулярок
 ]
